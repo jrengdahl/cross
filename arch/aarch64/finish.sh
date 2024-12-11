532a1aa901760b9640f322fb225f2cd3bfb3e6db
Jonathan Engdahl <jrengdahl@gmail.com>
2024-12-10 21:09:08 -0500

    Add etc/fstab and finish.sh

---
+++ arch/aarch64/finish.sh
@@ -0,0 +1,4 @@
+cp -r etc ../../aarch64-openmp-elf
+cp /bin/cygwin1.dll ../..//aarch64-openmp-elf/bin
+cp /bin/cygiconv-2.dll ../..//aarch64-openmp-elf/bin
+cp /bin/cyggcc_s-seh-1.dll ../..//aarch64-openmp-elf/bin
---
+++ arch/arm-cortexm33/finish.sh
@@ -0,0 +1,4 @@
+cp -r etc ../../arm-cortexm33-eabi
+cp /bin/cygwin1.dll ../..//arm-cortexm33-eabi/bin
+cp /bin/cygiconv-2.dll ../..//arm-cortexm33-eabi/bin
+cp /bin/cyggcc_s-seh-1.dll ../..//arm-cortexm33-eabi/bin
