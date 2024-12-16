a4d38151fcc226cf49c9bf3f49b1df273aa36c7a
Jonathan Engdahl <jrengdahl@gmail.com>
2024-12-16 12:01:37 -0500

    Change host compiler from Cygwin to MinGW usisng Canadian GCC build. This fixes the Cygwin path resolution issues.
    Also the toolchain is no longer dependent on Cygwin DLLs.
    The Cygwin fstab is no longer needed.
    The toolchain uses C:/ instead of /cygdrive/c/.
    
    Renamed finish.sh to build.sh and fixed it.
    
    Added LICENSE.txt and GNU licenses.
    
    At this point only cortexm7 and cortexm33 toolchains have been updated
    and tested.

---
+++ arch/arm-cortexm33/build.sh
@@ -0,0 +1,12 @@
+#!bash
+
+# Use crosstool-ng to build the cross toolchain, under control of the .config file.
+ct-ng build
+
+# Copy the pthread DLL from the host Cygwin install. It is needed by the toolchain executables.
+cp c:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin/libwinpthread-1.dll c:/cross/arm-cortexm33-eabi/bin
+
+# Copy omp.h from the host cygwin install. It might not be the exact matching version for the toolchain that was built,
+# but for the subset of OpenMP supported by bare metal it is probably good enough.
+cp c:/cygwin64/lib/gcc/x86_64-w64-mingw32/12/include/omp.h c:/cross/arm-cortexm33-eabi/lib/gcc/arm-cortexm33-eabi/14.2.0/include
+
--- arch/arm-cortexm7/finish.sh
+++
@@ -1,4 +0,0 @@
-cp -r etc ../../arm-cortexm7-eabi
-cp /bin/cygwin1.dll ../..//arm-cortexm7-eabi/bin
-cp /bin/cygiconv-2.dll ../..//arm-cortexm7-eabi/bin
-cp /bin/cyggcc_s-seh-1.dll ../..//arm-cortexm7-eabi/bin
