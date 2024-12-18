678f878994dd0157912cc1219425e0bfe5696deb
Jonathan Engdahl <engdahl@windstream.net>
2024-12-17 23:32:09 -0500

    Change .config files so that cross compiler and binutils are linked static. No non-Windows DLLs required.

--- arch/arm-cortexm4/build.sh
+++ arch/arm-cortexm4/build.sh
@@ -5,5 +5,5 @@ ct-ng build
 
 # Copy omp.h from the host cygwin install. It might not be the exact matching version for the toolchain that was built,
 # but for the subset of OpenMP supported by bare metal it is probably good enough.
-cp c:/cygwin64/lib/gcc/x86_64-w64-mingw32/12/include/omp.h c:/cross/arm-cortexm33-eabi/lib/gcc/arm-cortexm33-eabi/14.2.0/include
+cp c:/cygwin64/lib/gcc/x86_64-w64-mingw32/12/include/omp.h c:/cross/arm-cortexm4-eabi/lib/gcc/arm-cortexm4-eabi/14.2.0/include
 
